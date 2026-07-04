2e749822bf695f20e45d7e90a5c9ac0eec35ecb2
Sohit <sohitmk@gmail.com>
2026-07-04 23:28:42 +0530

    Update LogEventFuzzer.java

--- fuzz/LogEventFuzzer.java
+++ fuzz/LogEventFuzzer.java
@@ -1,9 +1,8 @@
-import io.aegiswire.core.ParseException;
 public class LogEventFuzzer {
     public static void fuzzerTestOneInput(byte[] data) throws Exception {
         try {
             new io.aegiswire.log.LogParser().parse(data);
-        } catch (ParseException | IllegalArgumentException expected) {
+        } catch (IllegalArgumentException expected) {
         }
     }
 }
